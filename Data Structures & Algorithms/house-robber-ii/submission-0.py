8163a62b55592ffa1034361169f4885990112ca8
Varsha Kumbar <varshakumbar9@gmail.com>
2026-09-13 13:27:03 +0530

    Add: house-robber-ii - submission-0

---
+++ Data Structures & Algorithms/house-robber-ii/submission-0.py
@@ -0,0 +1,14 @@
+class Solution:
+    def rob(self, nums: List[int]) -> int:
+       
+        return max(nums[0], self.helper(nums[1:]), self.helper(nums[:-1])) 
+
+    def helper(self, nums):
+        r1, r2 = 0, 0
+
+        for n in nums:
+            temp = max(n+r1, r2)
+            r1 = r2
+            r2 = temp
+        return r2 
+
